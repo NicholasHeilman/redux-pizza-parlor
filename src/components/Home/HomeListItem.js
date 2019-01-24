8f65461697c147f742fed92c0198e38d6071f523
Bode Falade <bode.falade@outlook.com>
2019-01-24 16:16:47 -0600

    reducer and conditional rendering working

--- src/components/Home/HomeListItem.js
+++ src/components/Home/HomeListItem.js
@@ -4,8 +4,40 @@ import { connect } from 'react-redux'
 
 
 class HomeListItem extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            add: true,
+        };
+    }
     
+    addOrRemove = () => {
+        if(this.state.add === true){
+            return <button onClick={this.handleAddClick}>Add</button>
+        }
+        else{
+            return <button onClick={this.handleRemoveClick}>Remove</button>
+        }
+    }
 
+    handleAddClick = () => {
+        console.log(this.props.pizza);
+        const action = {type: 'SET_PIZZA', payload: this.props.pizza};
+        this.props.dispatch(action);
+        this.setState({
+            add: false,
+        })
+
+    }
+
+    handleRemoveClick = () => {
+        console.log(this.props.pizza);
+        const action = {type: 'REMOVE_PIZZA', payload: this.props.pizza};
+        this.props.dispatch(action);
+        this.setState({
+            add: true,
+        })
+    }
    
     
 
@@ -14,10 +46,11 @@ class HomeListItem extends Component{
             <div>
                 <li><p>{this.props.pizza.name}</p></li>
                 <li><p>{this.props.pizza.price}</p></li>
-                <button>Add</button>
+                {this.addOrRemove()}
             </div>
         )
     }
 }
-// const mapReduxStoreToProps = (reduxStore) => ( {reduxStore: reduxStore});
-export default connect() (HomeListItem);
+
+const mapReduxStoreToProps = (reduxStore) => ( {reduxStore: reduxStore});
+export default connect(mapReduxStoreToProps)(HomeListItem);
