3ce07852e3625d318483430ee89fbecff6fb1eaa
Bode Falade <bode.falade@outlook.com>
2019-01-24 11:25:13 -0600

    add GET route for pizza display.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import axios from 'axios';
+
 
 class Home extends Component{
+
+    componentDidMount(){
+        this.refreshPizzaPage();
+    }
+
+    refreshPizzaPage = () => {
+
+        axios({
+            method: 'GET',
+            url: '/api/pizza'
+        }).then((response) => {
+            console.log('response in GET: ', response);
+            const action = {
+                type: 'SET_PIZZA',
+                payload: response.data
+            };
+            this.props.dispatch(action);
+        }).catch((error) => {
+            alert('Error in GET: ', error)
+        });
+    }
+
     render(){
         return(
             <div>
@@ -10,4 +35,4 @@ class Home extends Component{
     }
 }
 
-export default Home;
+export default connect() (Home);
