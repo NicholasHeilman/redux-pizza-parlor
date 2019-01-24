a7c435b7323db952ff8329f87303d095564eb3bd
Cody Troop <codytroop@yahoo.com>
2019-01-24 12:02:55 -0600

    Basic setup for customer info page, does not pass info anywhere yet

--- src/components/CustomerInfo/CustomerInfo.js
+++ src/components/CustomerInfo/CustomerInfo.js
@@ -5,14 +5,65 @@ class CustomerInfo extends Component{
         super(props);
         this.state = {
             name: '',
-            streetAddress: '',
-            
+            address: '',
+            city: '',
+            zip: '',
+            type: 'pick_up',
         }
+
     }
+
+
+    //////////////////////////////////////Handle state changes from inputs
+    handleNameChange = (event) => {
+        this.setState({
+            name: event.target.value,
+        });
+    }
+    handleAddressChange = (event) => {
+        this.setState({
+            address: event.target.value,
+        });
+    }
+    handleCityChange = (event) => {
+        this.setState({
+            city: event.target.value,
+        });
+    }
+    handleZipChange = (event) => {
+        this.setState({
+            zip: event.target.value,
+        });
+    }
+    handleDeliveryClick = () => {
+        this.setState({
+            type: 'delivery',
+        });
+    }
+    handlePickUpClick = () => {
+        this.setState({
+            type: 'pick_up',
+        });
+    }
+    ///////////////////////////////////////End state change handlers
+
 render(){
     return(
         <div>
-            <h1>Customer Info</h1>
+            <h1>Step 2: Customer Info</h1>
+            <br />
+            <input onChange={this.handleNameChange} type="text" placeholder="Name" />
+            <br />
+            <input onChange={this.handleAddressChange} type="text" placeholder="Street Address" />
+            <br />
+            <input onChange={this.handleCityChange} type="text" placeholder="City name" />
+            <br />
+            <input onChange={this.handleZipChange} type="text" placeholder="Zip Code" />
+            <br />
+
+            <button onClick={this.handleDeliveryClick}>Delivery</button>
+            <button onClick={this.handlePickUpClick}>Pick Up</button>
+
         </div>
     )
 }
