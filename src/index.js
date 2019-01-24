f148dc752d991b1213802102f25b135bad404097
Bode Falade <bode.falade@outlook.com>
2019-01-24 12:00:19 -0600

    added index info

--- src/index.js
+++ src/index.js
@@ -3,6 +3,46 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
+//import Redux
+import {
+    createStore,
+    combineReducers,
+    applyMiddleware
+} from 'redux';
+import {
+    Provider
+} from 'react-redux';
+import logger from 'redux-logger';
 
+//Pizza Reducer
+const pizzaReducer = (state = [], action) => {
+    if (action.type === 'SET_PIZZA'){
+        const pizzaToAdd = action.payload
+        return [...state, pizzaToAdd]
+    }
+        return state;
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Customer Reducer
+const customerReducer = (state = {}, action) => {
+
+    return state;
+}
+
+//Total Cost Reducer
+const totalReducer = (state = 0, action) => {
+
+    return state;
+}
+
+//Create store
+const storeInstance = createStore(
+    combineReducers({
+        pizzaReducer,
+        customerReducer,
+        totalReducer,
+    }),
+    applyMiddleware(logger),
+);
+
+ReactDOM.render(<Provider store ={storeInstance}>< App/></Provider>, document.getElementById('root'));
