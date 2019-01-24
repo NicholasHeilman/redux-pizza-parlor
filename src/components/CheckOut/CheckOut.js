d84521d55f04bf4465a10957215de5f9236154a8
James Tucker <jamesctucker@protonmail.com>
2019-01-24 16:45:05 -0600

    this will complete partial POST functionality

--- src/components/CheckOut/CheckOut.js
+++ src/components/CheckOut/CheckOut.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 import './../App/App.css';
 import { connect } from 'react-redux';
-import CustomerInfo from './../CustomerInfo/CustomerInfo.js';
+// import CustomerInfo from './../CustomerInfo/CustomerInfo.js';
+import axios from 'axios';
 
 
 class CheckOut extends Component {
 
+
+    postPizza = () => {
+        const info =
+        {
+            customer_name: this.props.reduxStore.customerReducer.name,
+            street_address: this.props.reduxStore.customerReducer.address,
+            city: this.props.reduxStore.customerReducer.city,
+            zip: this.props.reduxStore.customerReducer.zip,
+            type: this.props.reduxStore.customerReducer.type,
+            total: this.props.reduxStore.totalReducer.total,
+            pizzas: this.props.reduxStore.pizzaReducer.pizzas
+        }
+        axios({
+            method: 'POST',
+            url: '/api/pizza',
+            data: info
+        }).then((response) => {
+            console.log(response);
+
+        }).catch((error) => {
+            // console.log('error in GET, ', error);
+
+            alert('Error in POST: ', error)
+        });
+    }
+
     setCheckout = () => {
+        this.postPizza();
         this.props.history.push('/');
     }
 
@@ -30,6 +58,12 @@ class CheckOut extends Component {
 
                     </tbody>
                 </table>
+                <div>
+                    <h3 id="order-total-checkout-pg">Total: {this.props.reduxStore.totalReducer}</h3>
+                </div>
+                <br />
+                <br />
+                <br />
                 <br />
                 <div>
                     <button id="checkout-next" onClick={this.setCheckout}>Checkout</button>
