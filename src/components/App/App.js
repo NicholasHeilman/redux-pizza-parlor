27504f33f672df2102bc4a933b5f2fabdf22a3b5
Tang Yang <ty4081097@gmail.com>
2019-01-25 09:59:58 -0600

    update readMe.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import './App.css';
 import Home from './../Home/Home'
 import CustomerInfo from './../CustomerInfo/CustomerInfo'
 import CheckOut from './../CheckOut/CheckOut'
 import Admin from './../Admin/Admin'
 
-import { HashRouter as Router, Route, NavLink } from 'react-router-dom';
+import { HashRouter as Router, Route,} from 'react-router-dom';
 
 class App extends Component {
   render() {
